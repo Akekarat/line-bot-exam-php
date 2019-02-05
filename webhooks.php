cd1f9d301627c8d07df6fc3c28b3f3786b99fed3
Akekarat Pattrasitidecha <akekarat@themobilelife.com>
2019-02-05 10:49:19 +0700

    update token

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'u7eNieLpXmmvzYo9pI6+t2+z+427Em1fndikrgiiGSAvsYvXCiYhco6110kKZpjjZcfZRRvJO3RJy8UGfl/SrwQaebUD/FvlPwvjRUflY4fi3HTX8cppzXTr+aQ54Y3iI03l6WUOOa/cRiPKjZfJHwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
